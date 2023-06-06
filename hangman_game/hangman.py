c0fbdb149af9dd4e75c713ddea59468e7c1fd100
midedev <ibrahimayomide330@gmail.com>
2023-06-06 13:46:25 +0100

    add game rules as comment

---
+++ hangman_game/hangman.py
@@ -0,0 +1,9 @@
+# game where user guess the hidden word.
+# looks like this __p_y => happy
+# open words txt and use it as dictionary
+# clean it up and store it
+# give user number of words to guess and increase
+# difficulty in ranges as user suceed
+# user also has 6 life to guess words in each stage
+
+# START
