7ac96a3ee964feac9d1eb8dc657f07390f621c18
midedev <ibrahimayomide330@gmail.com>
2023-06-07 17:37:14 +0100

    create func to hide word letters randomly

--- hangman_game/hangman.py
+++ hangman_game/hangman.py
@@ -1,11 +1,3 @@
-# game where user guess the hidden word.
-# looks like this __p_y => happy
-# open words txt and use it as dictionary
-# clean it up and store it
-# give user number of words to guess and increase
-# difficulty in ranges as user suceed
-# user also has 6 life to guess words in each stage
-
 # START
 import random
 
@@ -51,13 +43,46 @@ def generate_secret_word(word_difficulty = range(3,5)):
 
 secret_word = 'Good'
 
-no_of_hidden_letters = round(len(secret_word)/2)
 
-secret_list = list(secret_word)
-for i in range(no_of_hidden_letters):
-    # hidden_letter = 
+
+
+# a dict that holds every letter in secret word
+# and count their number of occurence
+secret_dict = {}
+for char in secret_word:
+    if char in secret_dict:
+        secret_dict[char] += 1
+    else:
+        secret_dict[char] = 1
+
+
+def hide_word_parts(secret_word):
+    # convert secret word into a list to randomly
+    # select parts to hide
+    secret_list = list(secret_word)
+
+    # determine how many letters to hide
+    no_of_hidden_letters = round(len(secret_word)/2)
+
+    # randomly select and hide the letters
+    prev_index = None
+    for i in range(no_of_hidden_letters):
+        hidden_index = random.randrange(len(secret_list))
+        
+        # to prevent the code from choosing same index twice
+        if hidden_index == prev_index:
+            hidden_index = random.randrange(len(secret_list))
+        else:
+            prev_index = hidden_index
+        
+        secret_list[hidden_index] = "_"
     
-    print(random.choice(secret_list))
+    return secret_list
+
+
+
+
+print(secret_list)
     
 
 
