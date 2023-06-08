fd58f06497a703446d9d668c191d6853a79dcc7c
midedev <ibrahimayomide330@gmail.com>
2023-06-08 16:29:21 +0100

    create hangman logic

--- hangman_game/hangman.py
+++ hangman_game/hangman.py
@@ -52,15 +52,12 @@ def generate_secret_word(player_level):
           
     
 # function to hide parts of the word
-def hide_word_parts(secret_word):
-    # convert secret word into a list to randomly
-    # select parts to hide
-    secret_list = list(secret_word)
+def hide_word_parts(secret_list):
 
     if len(secret_list) == 3:
-        no_of_hidden_letters = 2
+        no_of_hidden_letters = 1
     else: 
-        no_of_hidden_letters = round(len(secret_word)/2)
+        no_of_hidden_letters = round(len(secret_list)/2)
 
     # randomly select and hide the letters
     prev_index = None
@@ -74,28 +71,44 @@ def hide_word_parts(secret_word):
             prev_index = hidden_index
         
         secret_list[hidden_index] = "_"
-    
     return " ".join(secret_list)
 
 
 # display word to the player
-def display_next_word(secret_word):
-    print(f"Guess the following word: \n{hide_word_parts(secret_word)}")
+def display_next_word(word):
+    print(f"Guess the following word: \n{word}")
 
 
 # determine if guess is right or wrong
-def player_guess(secret_word, displayed_word):
+def player_guess(secret_word, secret_list, secret_dict, player_input):
     
-    # a dict that holds every letter in secret word
-    # and count their number of occurence
-    secret_dict = {}
+    
     for char in secret_word:
         if char in secret_dict:
             secret_dict[char] += 1
         else:
             secret_dict[char] = 1
-    
+ 
+    # if letter already printed on screen, remove the 
+    # from guess dictionary
+    for letter in secret_list:
+        if letter in secret_dict:
+            secret_dict[letter] -= 1
+      
     # verify player guess
+    try:
+        if secret_dict[player_input] == 0 : return False
+        
+        if secret_dict[player_input] > 0:
+            for index, letter in enumerate(secret_list):
+                if letter == '_' and player_input == secret_word[index]:
+                    secret_list[index] = player_input
+                    secret_dict[player_input] -= 1
+                    return secret_list           
+    except KeyError:
+        return False
+
+ 
 
 # collect player input
 def player_input():
@@ -104,11 +117,13 @@ def player_input():
         player_input = input("Enter Your Next Guess: ").lower()
     return player_input
 
+
+
 # main game
 def hangman():
     # Set default variables
     user_life = 6
-    player_level = 0
+    player_level = 1
 
     # Game welcome screen
     print("*******************************")
@@ -116,16 +131,72 @@ def hangman():
     print("Rules are as Follows.\nYou'll be given a word with missing letters.\nYour task is to guess the letters correctly\nin 6 tries or you loose")
     print("*******************************")
     print("\n")
+    print(f"Level {player_level}")
     
     # generate the next word
-    secret_word = generate_secret_word(player_level)
+    secret_word = generate_secret_word(player_level).lower()
+    
+    # convert secret word into a list
+    secret_list = list(secret_word)
+    
+    # a dict that holds every letter in generated 
+    # secret word plus their number of occurence
+    secret_dict = {}
+    
+    # hide some parts and store it
+    displayed_word = hide_word_parts(secret_list)
     
     # display generated word with hidden parts
-    display_next_word(secret_word)
+    display_next_word(displayed_word)
     
-    player_input()
+    player_inp = player_input()
+    player_input_verify = player_guess(secret_word, secret_list ,secret_dict, player_inp)
     
+    game_on = True
     
+    while game_on:   
+        if player_input_verify:
+            if '_' not in player_input_verify:
+                print("You Guessed the Word Correctly 🎉")
+                print(f'{secret_word} 🎯')
+                print("*********************************")
+                player_level += 1
+                user_life = 6
+                print(f"Level {player_level} 😇")
+                # generate new word for next level
+                secret_word = generate_secret_word(player_level).lower()
+                # convert secret word into a list
+                secret_list = list(secret_word)
+                # empty secret dict
+                secret_dict = {}
+                # hide some parts and store it
+                displayed_word = hide_word_parts(secret_list)
+                # display generated word with hidden parts
+                display_next_word(displayed_word)
+                # collect player input
+                player_inp = player_input()
+                # verify if input is correct
+                player_input_verify = player_guess(secret_word, secret_list ,secret_dict, player_inp)  
+            else:
+                print(f"{''.join(player_input_verify)} ✅")
+                player_inp = player_input()
+                player_input_verify = player_guess(secret_word, secret_list ,secret_dict, player_inp)
+                
+        else:   
+            user_life -= 1
+            if user_life == 0:
+                print("------------")
+                print("You Loose ☹️")
+                print(f'The word is: {secret_word}')
+                game_on = False
+            else:
+                print("---------------")
+                print("Wrong Guess ❌")
+                print(f'Remaining Tries: {user_life}')
+                player_inp = player_input()
+                player_input_verify = player_guess(secret_word, secret_list ,secret_dict, player_inp)
+
+            
         
     
 
---
+++ hangman_game/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+ player_input in secret_dict and secret_dict[player_input] > 0:
+    #     for letter in displayed_word:
+    #         if letter == '_' and 
