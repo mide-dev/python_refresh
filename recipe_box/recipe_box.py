d13a2806c4b389e0f1e8f22dc22bc571279c5853
midedev <ibrahimayomide330@gmail.com>
2023-06-17 16:29:52 +0100

    refactor code into reuseable function - navigate_directory

--- recipe_box/recipe_box.py
+++ recipe_box/recipe_box.py
@@ -5,9 +5,7 @@ class RecipeBox:
     # define constructor and implement prgram flow
     def __init__(self):
         # path
-        __path = "C:/Users/Ayomide/Desktop/Development/Python/Python_refresh/recipe_box/Recipes"
-        # convert path type
-        __recipe_dir = Path(__path)
+        __recipe_dir = Path("C:/Users/Ayomide/Desktop/Development/Python/Python_refresh/recipe_box/Recipes")
         # count how many recipe files available in recipe dir and sub-dir
         __recipe_count = 0
         for recipe in list(__recipe_dir.glob('**/*.txt')):
@@ -39,29 +37,44 @@ class RecipeBox:
         # print user options
         self.print_options(menu_options)
         # accept user input to pick an option
-        user_choice = self.select_option(menu_options)
+        user_menu_choice = self.select_option(menu_options)
         
-        # 
-        if user_choice == 1:
-            pass
+        # if user wants to read a recipe
+        if user_menu_choice == 1:
+            print('\nChoose Category of recipe to read')
+            user_category_choice = self.navigate_directory(__recipe_dir)
+            print(user_category_choice)
+            
+                    
+            
+            
         
-    # func to print user options  
+    # print user options  
     def print_options(self, options:list) -> None:
         # print out list for user
         for index, choice in enumerate(options):
             print(f'{choice} - {index + 1}')
-        
+    
+    # return user choice 
     def select_option(self, options:list) -> int:
         option = None
         while option not in range(len(options) + 1):
             option = int(input('Choose one of the Options: '))
         return option             
         
-    # func to return a folder
-    def choose_category(self, categories: list) -> Path:
-        # goes into the folders and select a cat
-        # returns a folder with list of recipe
-        pass
+    # func to return a folder path
+    def navigate_directory(self, base_directory:Path) -> Path:
+        # store all folder name in base_dir as a list
+        all_folders =  [item.name for item in base_directory.iterdir() if item.is_dir()]
+        # print all available folder name for user to choose from
+        self.print_options(all_folders)
+        # accept input on what folder user needs to access
+        user_choice = self.select_option(all_folders)
+        # if folder name is same as user choice
+        # return new path
+        for index, category in enumerate(all_folders):
+            if index + 1 == user_choice:
+                return Path(f'{base_directory}\{all_folders[index]}')
     
     def choose_recipe(self, category, recipe_name):
         # return a recipe path
