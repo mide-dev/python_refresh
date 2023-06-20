b22130ebf663d68bb7ea87961a52ddfc5d44b441
midedev <ibrahimayomide330@gmail.com>
2023-06-20 20:51:23 +0100

    refactor code into functions

--- recipe_box/recipe_box.py
+++ recipe_box/recipe_box.py
@@ -31,7 +31,7 @@ class RecipeBox:
         print(textwrap.dedent(_welcome_str))
         
         # list of menu options
-        _menu_options =  ['read recipe', 'create recipe', 'create category', 'delete recipe', 'exit']
+        _menu_options =  ['read recipe', 'create recipe', 'create category', 'delete recipe', 'delete category', 'exit']
         # list of categories in directory
         # categories = [category.name for category in __recipe_dir.iterdir() if category.is_dir()]
         # print user options
@@ -39,48 +39,25 @@ class RecipeBox:
         # accept user input to pick an option
         user_menu_choice = self._select_option(_menu_options)
         
-        # if user wants to read a recipe
+        # let user read recipe
         if user_menu_choice == 1:
-            print('\nChoose Category of recipe to read')
-            # navigate to user recipe category
-            user_category_choice = self._navigate_directory(_recipe_dir)
-            print('\nChoose recipe to read')
-            # navigate to user recipe
-            user_recipe_choice = self._navigate_directory(user_category_choice)
-            # Open the file in read mode
-            with open(user_recipe_choice, 'r') as file:
-                # Read the contents of the file
-                print(f'\n{file.read()}\n')
+            pass
+        
         # let user create new recipe
         if user_menu_choice == 2:
-            print('\nChoose Category to create recipe in')
-            recipe_category = self._navigate_directory(_recipe_dir)
-            # check if chosen option is a valid folder
-            if recipe_category.is_dir():
-                # collect recipe name from user
-                recipe_name = input('\nType new recipe name: ')
-                # concatenate chosen category and recipe name into single path
-                new_recipe_path = Path(f'{recipe_category}/{recipe_name}.txt')
-                # prevent duplicate recipe name
-                while new_recipe_path.exists():
-                    print('\nRecipe name already exists')
-                    recipe_name = input('Type new recipe name: ')
-                # store recipe content
-                recipe_content = input('\nType recipe content: ')
-                # create new recipe file and write content to it
-                with open(new_recipe_path, 'w') as new_recipe:
-                    new_recipe.write(recipe_content)
-                print('SUCCESS🎉 - Recipe Created')
+            pass
+       
         # let user create new recipe category
         if user_menu_choice == 3:
-            category_name = input('\nEnter Category Name: ')
-            new_category_path = Path(f'{_recipe_dir}/{category_name}')
-            while new_category_path.exists():
-                    print('\Category already exists')
-                    category_name = input('Type new Category name: ')
-            new_category_path.mkdir(parents=True, exist_ok=True)
-            print('SUCCESS🎉 - Folder Created')
-         
+            pass
+       
+        # let user delete recipes
+        if user_menu_choice == 4:
+            pass
+       
+        # let user delete category
+        if user_menu_choice == 5:
+            pass
             
             
         
@@ -111,57 +88,74 @@ class RecipeBox:
             if index + 1 == user_choice:
                 return Path(f'{base_directory}\{all_folders[index]}')
     
-    def choose_recipe(self, category, recipe_name):
-        # return a recipe path
-        pass
+    def read_recipe(self, recipe_path:Path) -> None:
+        # if user wants to read a recipe
+        print('\nChoose Category of recipe to read')
+        # navigate to user recipe category
+        user_category_choice = self._navigate_directory(recipe_path)
+        print('\nChoose recipe to read')
+        # navigate to user recipe
+        user_recipe_choice = self._navigate_directory(user_category_choice)
+        # Open the file in read mode
+        with open(user_recipe_choice, 'r') as file:
+            # Read the contents of the file
+            print(f'\n{file.read()}\n')
     
-    def read_recipe(self, category, recipe_name):
-        # call choose_recipe and read the content
-        pass
+    def create_recipe(self, recipe_path:Path) -> None:
+        print('\nChoose Category to create recipe in')
+        recipe_category = self._navigate_directory(recipe_path)
+        # check if chosen option is a valid folder
+        if recipe_category.is_dir():
+            # collect recipe name from user
+            recipe_name = input('\nType new recipe name: ')
+            # concatenate chosen category and recipe name into single path
+            new_recipe_path = Path(f'{recipe_category}/{recipe_name}.txt')
+            # prevent duplicate recipe name
+            while new_recipe_path.exists():
+                print('\nRecipe name already exists')
+                recipe_name = input('Type new recipe name: ')
+            # store recipe content
+            recipe_content = input('\nType recipe content: ')
+            # create new recipe file and write content to it
+            with open(new_recipe_path, 'w') as new_recipe:
+                new_recipe.write(recipe_content)
+            print('SUCCESS🎉 - Recipe Created')
     
-    def create_recipe(self, category, recipe_name, content):
-        # run choose_category() for user 
-        # to pic a category
-        # collect name and content of new recipe
-        # append to cat folder
-        pass
+    def create_category(self, category_path:Path) -> None:
+        category_name = input('\nEnter Category Name: ')
+        new_category_path = Path(f'{category_path}/{category_name}')
+        while new_category_path.exists():
+            print('\Category already exists')
+            category_name = input('Type new Category name: ')
+        new_category_path.mkdir(parents=True, exist_ok=True)
+        print('SUCCESS🎉 - Folder Created')
     
-    def create_category(self, category_name):
-        # recieves a category name
-        # and creates it
-        pass
+    def delete_recipe(self, recipe_path:Path) -> None:
+        # locate the recipe to delete
+        category_to_delete_from = self._navigate_directory(recipe_path)
+        recipe_to_delete = self._navigate_directory(category_to_delete_from)
+        # delete the file
+        recipe_to_delete.unlink()
+        print('SUCCESS🎉 - Recipe Deleted')
     
-    def delete_recipe(self, category, recipe_name):
-        # call choose_recipe and delete the result
-        pass
+    def delete_category(self, category_path:Path) -> Path:
+        # choose directory to delete
+        print('\nChoose Category to delete')
+        category_to_delete = self._navigate_directory(category_path)
+        # if category contains recipe, then delete them first
+        if not len(list(category_to_delete.iterdir())) == 0:
+            for item in category_to_delete.glob('*'):
+                item.unlink()
+        # delete the directory itself
+        category_to_delete.rmdir()
+        print('SUCCESS🎉 - Category Deleted')
     
     def exit_program(self):
         # break out of the whole loop
         pass
    
+   
+   
 # initiate class
 recipe_box = RecipeBox()
-# list all folders in a dir
-# p = Path('./recipe_box/Recipes')
-# print ([x for x in p.iterdir() if x.is_dir()])
-
-# listing python files in the sub-dir
-# print(list(p.glob('**/*.txt')))
-
-# path boolean
-# print(p.exists())
-
-# read contents
-# with p.open() as f:
-#     print(f.readline())
-
-# print(str(p))
-# print(p.suffix)
-# print(p.suffixes)
-# print(p.name)
-# print(p.parent)
-# print(p.match('*.txt'))
-# PureWindowsPath('c:').joinpath('/Program Files')
-
-# print(Path.cwd)
 
